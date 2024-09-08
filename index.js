a7f55c42ac48a4ab677538b807f63d724c137586
KaoutharDraif <kaouthardraif@gmail.com>
2024-09-08 22:59:22 +0100

    newchanges

--- index.js
+++ index.js
@@ -2,6 +2,22 @@ const formRecette = document.querySelector('form');
 const Plat = document.getElementById('titre');
 const Détails = document.getElementById('Recette');
 const recetteContainer = document.getElementById('recetteContainer');
+const breakfastContainer = document.getElementById('breakfastContainer');
+const lunchContainer = document.getElementById('lunchContainer');
+const dinnerContainer = document.getElementById('dinnerContainer');
+const mealType = document.getElementById('mealType');
+const formMeal = document.querySelector('form');
+
+
+function cardDay() {
+    if(mealType.style.display === "none"){
+        mealType.style.display = "block";
+    }
+    else{
+        mealType.style.display = "none" ;
+    }
+}
+ 
 
 let recettes = [];
 
