97a9425f0c01d4ac756140b3e836cc11b89514c9
KaoutharDraif <kaouthardraif@gmail.com>
2024-09-08 20:01:34 +0100

    Delete button added

--- index.js
+++ index.js
@@ -23,9 +23,9 @@ function addRecette() {
 
 function displayRecettes() {
     recetteContainer.innerHTML = '';
-    recettes.forEach((recette, index) => { 
+    recettes.forEach((recette, index) => {
         const recetteElement = document.createElement('div'); //creates a new div element to display the recipe on the webpage
-        
+
 
         const Recipe = document.createElement('h3');
         Recipe.textContent = recette.titre;
@@ -42,32 +42,38 @@ function displayRecettes() {
 
         });
 
-        // const deleteButton = document.createElement('button');
-        // deleteButton.innerHTML = '<i class="fa-solid fa-trash"></i>';
-        // deleteButton.addEventListener('click', () => {
-        //     deleteRecipe(index);
-        // });
+        const deleteButton = document.createElement('button');
+        deleteButton.innerHTML = '<i class="fa-solid fa-trash"></i>';
+        deleteButton.addEventListener('click', () => {
+            deleteRecipe(index);
+        });
 
         recetteContainer.appendChild(recetteElement);
         recetteContainer.appendChild(editButton);
-        // recetteContainer.appendChild(deleteRecipe);
+        recetteContainer.appendChild(deleteButton);
     })
 }
 
+// Function to edit the title and the details of the recipe
 function editRecipe(index) {
+    // To edit the recipe's title
     const newtitleRecipe = prompt('Modifie le titre de la recette:', recettes[index].titre);
     if (newtitleRecipe !== null && newtitleRecipe !== '') {
         recettes[index].titre = newtitleRecipe;
-        displayRecettes(); 
+        displayRecettes();
     }
+    // To edit the recipe's details
     const newRecipe = prompt('Modifie votre recette:', recettes[index].détail);
     if (newRecipe !== null && newRecipe !== '') {
         recettes[index].détail = newRecipe;
-        displayRecettes(); 
+        displayRecettes();
     }
 }
 
-
+function deleteRecipe(index) {
+    recettes.splice(index, 1);
+    displayRecettes();
+}
 
 
 
